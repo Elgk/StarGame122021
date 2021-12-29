4dea6b085790cccf820ccd39efe7f1c424e10e89
Elgk <lena.elena-kuznetzova2016@yandex.ru>
2021-12-29 12:42:05 +0400

    Homework lesson2

--- core/src/com/star/app/game/GameController.java
+++ core/src/com/star/app/game/GameController.java
@@ -5,12 +5,14 @@ public class GameController {
     private Hero hero;
     private Background background;
     private BulletController bulletController;
+    private AsteroidController asteroidController;
 
     public GameController() {
         this.background = new Background(this);
         this.asteroid = new Asteroid();
         this.hero = new Hero(this);
         this.bulletController = new BulletController();
+        this.asteroidController = new AsteroidController();
     }
 
     public Asteroid getAsteroid() {
@@ -29,17 +31,30 @@ public class GameController {
         return bulletController;
     }
 
+    public AsteroidController getAsteroidController() {
+        return asteroidController;
+    }
+
     public void update(float dt){
         background.update(dt);
         asteroid.update(dt);
         hero.update(dt);
         bulletController.update(dt);
-    //    checkCollisions();
+        asteroidController.update(dt);
+        checkCollisions();
     }
 
     private void checkCollisions() {
         for (int i = 0; i < bulletController.getActiveList().size(); i++) {
-            Bullet b = bulletController.getActiveElement();
+           Bullet bullet = bulletController.getActiveList().get(i);
+            for (int j = 0; j < asteroidController.getActiveList().size(); j++) {
+                Asteroid asd = asteroidController.getActiveList().get(j);
+                if (asd.getPosition().dst(bullet.getPosition()) < 130){
+                    bullet.deactivate();
+                    asteroidController.free(j);
+                }
+            }
         }
+
     }
 }
